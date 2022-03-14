7a10d0777c2bab1852742102e05aab894392759a
cristianodenis <cristianodenis62@gmail.com>
2022-03-13 21:10:20 -0300

    atualizando frontend

--- todo/src/services/api.js
+++ todo/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //10.0.2.2
-    baseURL: 'http://192.168.1.107:3333'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
